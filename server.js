c08927f7434f78012493413c8b57c6c00762ae28
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-12-22 16:44:58 +0500

    ready project

---
+++ server.js
@@ -0,0 +1,26 @@
+const ronin 		= require( 'ronin-server' )
+const database  = require( 'ronin-database' )
+const mocks 		= require( 'ronin-mocks' )
+
+async function main() {
+
+    try {
+    await database.connect( process.env.CONNECTIONSTRING )
+    
+    const server = ronin.server({
+            port: process.env.SERVER_PORT
+        })
+        server.use( '/foo', (req, res) => {
+            return res.json({ "foo": "bar" })
+          })
+        server.use( '/', mocks.server( server.Router()) )
+
+    const result = await server.start()
+        console.info( result )
+    
+    } catch( error ) {
+        console.error( error )
+    }
+}
+
+main()
